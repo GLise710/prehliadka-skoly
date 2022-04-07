404d9eddaa7d82bc079a76f5679edb0e7fb232f7
GLise710 <matejkozub124@gmail.com>
2022-04-07 22:59:03 +0200

    Dev update - new projection mechanism

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-function multiply (a, b) {
+function multiply (a, b){
 	const transpose = (a) => a[0].map((x, i) => a.map((y) => y[i]));
 	const dotproduct = (a, b) => a.map((x, i) => a[i] * b[i]).reduce((m, n) => m + n);
 	const result = (a, b) => a.map((x) => transpose(b).map((y) => dotproduct(x, y)));
 	return result(a, b);
+}
+
+// Defeats the remainder !== modulo JS bug/feature
+function modulo(n, m){
+	return ((n % m) + m) % m;
+}
+
+function clamp(value, min, max){
+	if (value < min){
+		return min;
+	} else if (value > max){
+		return max;
+	} else {
+		return value;
+	}
 }
 
 class Projector {
@@ -86,8 +101,8 @@ class Projector {
 		this.width = width;
 		this.height = height;
 
-		this.fov = Projector.approximateFovFromAspectRatio(width / height) / 180 * Math.PI;
-		
+		this.fov = (Projector.approximateFovFromAspectRatio(width / height)) / 180 * Math.PI;
+
 		const m = width / height;
 
 		const squaredCtgHalfFov = (1 / Math.tan(this.fov / 2)) ** 2;
@@ -109,11 +124,104 @@ class Projector {
 		};
 	}
 
+	static projectPointV2(point, cameraVector){
+		const camY = Math.asin(cameraVector.y);
+		const ptY = Math.asin(point.y);
+		
+		// Projects the vector to the XZ plane, then normalizes it
+		// to properly compute asin and acos
+		const cameraNormXZ = 1 / Math.sqrt(cameraVector.x * cameraVector.x + cameraVector.z * cameraVector.z);
+		const camZnormalized = cameraVector.z * cameraNormXZ;
+		const camZ = Math.acos(camZnormalized);
+
+		const pointNormXZ = 1 / Math.sqrt(point.x * point.x + point.z * point.z);
+		const pointZnormalized = point.z * pointNormXZ;
+		const ptZ = Math.acos(pointZnormalized);
+
+		const camXZ = cameraVector.x > 0 ? camZ : 2 * Math.PI - camZ;
+		let ptXZ = point.x > 0 ? ptZ : 2 * Math.PI - ptZ;
+
+		// evil hack to fight circle wrapping
+		if (ptXZ < camXZ - 0.5 * this.fov){
+			while (ptXZ + 2 * Math.PI < camXZ + 0.5 * this.fov){
+				ptXZ += 2 * Math.PI;
+			}
+		}
+		if (ptXZ > camXZ + 0.5 * this.fov){
+			while (ptXZ - 2 * Math.PI > camXZ - 0.5 * this.fov){
+				ptXZ -= 2 * Math.PI;
+			}
+		}
+
+		// console.log("cam s", (camXZ - 1/2 * this.fov) * 180 / Math.PI);
+		// console.log("cam e", (camXZ + 1/2 * this.fov) * 180 / Math.PI);
+		// console.log("pt", ptXZ * 180 / Math.PI);
+
+		// console.log(camZ * 180 / Math.PI, camXZ * 180 / Math.PI);
+		// if (Date.now() % 1000 < 17){
+		// 	console.log((ptX - camXZ) / this.fov);
+		// }
+
+		let x = 0.5 - (ptXZ - camXZ) / this.fov;
+		let y = 0.5 - (ptY - camY) / (0.5 * Math.PI);
+
+
+		//console.log("tada", ptXZ - camXZ);
+		// correction
+		// const xDiff = Math.abs(0.5 - x);
+		// const xCorrection = Math.abs(xDiff * xDiff * 0.2);
+		// if (x < 0.5){
+		// 	x += xCorrection;
+		// } else {
+		// 	x -= xCorrection;
+		// }
+		
+		x -= 0.5;
+		// x *= -Math.sin(clamp(6 * x, -0.5, 0.5) - 1.5 * Math.PI) + 1.66;
+		const cx = clamp(x, -0.4, 0.4);
+		x *= 1.7 * cx * cx + 0.66 - this.width/this.height * 0.041;
+		
+		x += 0.5;
+		// const threshold = 0.33 * this.height / this.width;
+		// if (Math.abs(x) > threshold){
+		// 	console.log("correct");
+		// 	if (x < 0){
+		// 		x -= (x + threshold) ** 2;
+		// 	} else {
+		// 		x += (x - threshold) ** 2;
+		// 	}
+		// }
+
+
+		y -= 0.5;
+		const cy = clamp(y, -0.46, 0.46);
+		y *= 1.1 * cy * cy + 0.78;
+		y += 0.5;
+		// const yCorrection = Math.abs(0.07 * (0.5 - y) * (0.5 - y));
+
+		//console.log(point.x * cameraVector.x + point.y * cameraVector.y + point.z * cameraVector.z);
+
+		console.log(point.x * cameraVector.x + point.y * cameraVector.y + point.z * cameraVector.z);
+
+		return {
+			x: x * this.width,
+			y: y * this.height,
+			inSight: (point.x * cameraVector.x + point.y * cameraVector.y + point.z * cameraVector.z) > 0 // dot product of 2
+		}
+	}
+
 	static planeToScreen(point){
 		return {
 			x: (point.x + this.screenEdgeX) / (this.screenEdgeX * 2) * this.width,
 			y: (point.y + this.screenEdgeY) / (this.screenEdgeY * 2) * this.height
 		}
+	}
+	
+	static isInDrawCircle(screenPoint){
+		const x = (screenPoint.x - 0.5 * this.width);
+		const y = (screenPoint.y - 0.5 * this.height);
+		const r = Math.min(this.width, this.height) / 2;
+		return x * x + y * y < r * r;
 	}
 }
 
@@ -130,14 +238,18 @@ class Game {
 			this.videos[video.name] = new Video(video);
 		}
 		
-		this.goTo("vestibul");
+		this.goTo("vestibul", true);
 
-		setTimeout(() => {this.loop()}, 2000)
+		setTimeout(() => {this.loop()}, 1000)
 	}
 
-	goTo(name){
+	goTo(name, first=false){
+		if (!first){
+			this.currentScene.hideHitboxes();
+		}
+		
 		this.currentScene = this.scenes[name];
-
+		console.log(this.scenes);
 		document.querySelector("video").src = this.currentScene.source;
 
 		this.currentScene.displayHitboxes();
@@ -180,6 +292,12 @@ class Scene {
 			hitbox.display();
 		}
 	}
+	
+	hideHitboxes(){
+		for (const hitbox of this.hitboxes){
+			hitbox.hide();
+		}
+	}
 }
 
 class Video {
@@ -193,6 +311,12 @@ class Hitbox {
 		this.element = document.createElement("div");
 		this.element.classList.add("hitbox");
 
+		this.tooltip = source.tooltip;
+		this.tooltipElement = document.createElement("div");
+		this.tooltipElement.textContent = this.tooltip;
+		this.tooltipElement.classList.add("tooltip");
+		this.element.appendChild(this.tooltipElement);
+
 		this.point = {};
 		[this.point.x, this.point.y, this.point.z] = source.point;
 
@@ -201,7 +325,12 @@ class Hitbox {
 		this.point.y *= norm;
 		this.point.z *= norm;
 
-		console.log(this.point);
+		this.target = source.target;
+
+		this.element.addEventListener("click", () => {
+			game.goTo(this.target);
+			console.log(this.target);
+		});
 	}
 
 	display(){
@@ -212,19 +341,33 @@ class Hitbox {
 		this.element.remove();
 	}
 
-	update(rotationMatrix){
-		const rotated = Projector.rotatePoint(this.point, rotationMatrix);
-		this.element.hidden = Projector.v < rotated.z;
+	update(){
+		// const rotated = Projector.rotatePoint(this.point, rotationMatrix);
+		// this.element.hidden = Projector.v < rotated.z;
 
-		if (this.element.hidden){
-			return;
+		// if (this.element.hidden){
+		// 	return;
+		// }
+
+		// const inPlane = Projector.projectPoint(rotated);
+		// const onScreen = Projector.planeToScreen(inPlane);
+
+		const onScreen = Projector.projectPointV2(this.point, vr.cameraVector);
+
+		if (Projector.isInDrawCircle(onScreen)){
+			this.element.classList.remove("hidden");
+		} else {
+			this.element.classList.add("hidden");
 		}
 
-		const inPlane = Projector.projectPoint(rotated);
-		const onScreen = Projector.planeToScreen(inPlane);
+		if (onScreen.inSight){
+			this.element.style.display = "block";
+		} else {
+			this.element.style.display = "none";
+		}
 
 		this.element.style.left = onScreen.x + "px";
-		this.element.style.bottom = onScreen.y + "px";
+		this.element.style.top = onScreen.y + "px";
 	}
 }
 
